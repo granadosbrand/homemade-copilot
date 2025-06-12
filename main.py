6ed114c771a9b706e297294194806945a9a5a910
Diego Granados <diego.granados@sonepar.co>
2025-06-11 22:56:15 -0500

    CH1 C

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -12,12 +13,20 @@ if len(sys.argv) < 2:
     print("I'm still waiting your orders, master...")
     sys.exit(1)
 
-contents = sys.argv[1]
+user_prompt = sys.argv[1]
+verbose_mode = "--verbose" in sys.argv
 
-response_object = client.models.generate_content(model= model ,contents=contents)
+
+messages = [
+    types.Content(role = "user", parts = [types.Part(text = user_prompt)]),
+]
+
+response_object = client.models.generate_content(model= model ,contents=messages)
 
 print(response_object.text)
 print("...we'll work on your responses...")
 
-print(f"Prompt tokens: {response_object.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response_object.usage_metadata.candidates_token_count}")
+if verbose_mode:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response_object.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response_object.usage_metadata.candidates_token_count}")
